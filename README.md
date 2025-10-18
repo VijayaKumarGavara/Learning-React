584b033b9394d088b973c59e496b7f2084a09a30
VijayaKumarGavara <ggana6947@gamil.com>
2025-10-18 10:34:11 +0530

    Episode-02

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Learning React
+
+# Pracel
+    - Dev Build
+    - Local Server
+    - HMR - Hot Module Replacement
+    - File Watching Algorithm
+    - Caching
+    - Image Optimization
+    - Minification
+    - Compressing Source Codes
+    - Content Hashing
+    - Bundling
+    - Code Splitting
+    - Differential Bundling - To Support older browsers
+    - Diagnostic
+    - Error Handling
+    - Provides HTTPs
+    - Tree Shaking - removes the unusal code
+    
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client"
 /**
  * <div id='parent'> 
  *      <div id='child1'>
@@ -17,31 +18,31 @@
 */
 
 
-let content=React.createElement("div", {id:'parent'}, // <-- Creates a JS Object
-    [
-        React.createElement("div", {id:'child1'}, 
-        [
-            React.createElement("h1", {id:'n1'}, "Vijay"),
-            React.createElement("h2", {id:'r1'}, "SDE")
-        ]),
-        React.createElement("div", {id:'child2'}, 
-        [
-            React.createElement("h1", {id:'n2'}, "Yuvi"),
-            React.createElement("h2", {id:'r2'}, "SDE")
-        ])
-    ]
-);
+// let content=React.createElement("div", {id:'parent'}, // <-- Creates a JS Object
+//     [
+//         React.createElement("div", {id:'child1'}, 
+//         [
+//             React.createElement("h1", {id:'n1'}, "Vijay"),
+//             React.createElement("h2", {id:'r1'}, "SDE")
+//         ]),
+//         React.createElement("div", {id:'child2'}, 
+//         [
+//             React.createElement("h1", {id:'n2'}, "Yuvi"),
+//             React.createElement("h2", {id:'r2'}, "SDE")
+//         ])
+//     ]
+// );
 
 
 // const h = document.createElement("h1");
 // h.innerText = "Hello World2";
 // document.getElementById("root").appendChild(h);
 
-// let h2 = React.createElement("h2", {id:'heading'}, "Hello, World3");
+let h2 = React.createElement("h2", {id:'heading'}, "I am Learning React..!");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // console.log(h2);
 // root.render(h2);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(content); // <-- Converts the JS Object into actual html element.
+root.render(h2);
+// root.render(content); // <-- Converts the JS Object into actual html element.
