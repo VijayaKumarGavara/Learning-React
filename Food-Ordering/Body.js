77db7141626c9978b6e10c092c5e73e40f7648e9
VijayaKumarGavara <ggana6947@gamil.com>
2025-10-20 15:21:55 +0530

    Adding a feature Top Rated Food by using useSate & filter method

--- Food-Ordering/Body.js
+++ Food-Ordering/Body.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import { resList } from "./utils/mockData";
 
-
-
+// let list = resList; //.sort((b,a)=>b.resRating-a.resRating);
 const Body = () => {
+    const [list, setList] = useState(resList);
   return (
     <>
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          type="button"
+          onClick={() => {
+            let l = list.filter((res) => res.resRating > 4);
+            console.log(l.length);
+            setList(l);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
         {/* <RestaurantCard resName="Janata Food Court" cuisines="Fried Rice, Andhra, Indian" resRating="4.2 stars" delevieryTime="20 minutes"/>
                 <RestaurantCard resName="SVG Grand" cuisines="Biryani, Andhra" resRating="4.6 stars" delevieryTime="35 minutes"/>
@@ -20,11 +32,9 @@ const Body = () => {
         <RestaurantCard restaurant={resList[3]}/>
         <RestaurantCard restaurant={resList[4]}/> */}
 
-        {
-            resList.map((res, index)=>{
-                return <RestaurantCard  key={index} restaurant={res}/>
-            })
-        }
+        {list.map((res, index) => {
+          return <RestaurantCard key={index} restaurant={res} />;
+        })}
       </div>
     </>
   );
